28cfe61c335ffd6d8edfdf656a0c100d7a8134af
Luca Gazzardi <gazzardi.luca@gmail.com>
2026-02-24 20:03:50 +0100

    fixed fragments

--- AnimeTakusan/AnimeTakusan.Tests/TestHelpers/Fakers/AniListProviderFakers.cs
+++ AnimeTakusan/AnimeTakusan.Tests/TestHelpers/Fakers/AniListProviderFakers.cs
@@ -32,8 +32,8 @@ public static class AniListProviderFakers
     public static Faker<GetSeasonalAnime_Page_Media_Media> AniListSeasonalAnimeFaker => new Faker<GetSeasonalAnime_Page_Media_Media>()
         .CustomInstantiator(f => new GetSeasonalAnime_Page_Media_Media(
             id: f.Random.Number(1, 10000),
-            title: AniListTitleFaker.Generate(),
-            coverImage: AniListCoverImageFaker.Generate(),
+            title: AniListSeasonalTitleFaker.Generate(),
+            coverImage: AniListSeasonalCoverImageFaker.Generate(),
             startDate: null,
             endDate: null,
             bannerImage: f.Image.PlaceholderUrl(1900, 400),
@@ -49,14 +49,14 @@ public static class AniListProviderFakers
             isAdult: f.Random.Bool(),
             averageScore: f.Random.Number(0, 100),
             nextAiringEpisode: null,
-            studios: AniListStudiosFaker.Generate()
+            studios: AniListSeasonalStudiosFaker.Generate()
         ));
 
     public static Faker<GetBrowseSection_Season_Media_Media> AniListBrowseSeasonAnimeFaker => new Faker<GetBrowseSection_Season_Media_Media>()
         .CustomInstantiator(f => new GetBrowseSection_Season_Media_Media(
             id: f.Random.Number(1, 10000),
-            title: AniListTitleFaker.Generate(),
-            coverImage: AniListCoverImageFaker.Generate(),
+            title: AniListBrowseSeasonTitleFaker.Generate(),
+            coverImage: AniListBrowseSeasonCoverImageFaker.Generate(),
             startDate: null,
             endDate: null,
             bannerImage: f.Image.PlaceholderUrl(1900, 400),
@@ -72,14 +72,14 @@ public static class AniListProviderFakers
             isAdult: f.Random.Bool(),
             averageScore: f.Random.Number(0, 100),
             nextAiringEpisode: null,
-            studios: AniListStudiosFaker.Generate()
+            studios: AniListBrowseSeasonStudiosFaker.Generate()
         ));
 
     public static Faker<GetBrowseSection_NextSeason_Media_Media> AniListBrowseNextSeasonAnimeFaker => new Faker<GetBrowseSection_NextSeason_Media_Media>()
         .CustomInstantiator(f => new GetBrowseSection_NextSeason_Media_Media(
             id: f.Random.Number(1, 10000),
-            title: AniListTitleFaker.Generate(),
-            coverImage: AniListCoverImageFaker.Generate(),
+            title: AniListBrowseSeasonTitleFaker.Generate(),
+            coverImage: AniListBrowseSeasonCoverImageFaker.Generate(),
             startDate: null,
             endDate: null,
             bannerImage: f.Image.PlaceholderUrl(1900, 400),
@@ -95,14 +95,14 @@ public static class AniListProviderFakers
             isAdult: f.Random.Bool(),
             averageScore: f.Random.Number(0, 100),
             nextAiringEpisode: null,
-            studios: AniListStudiosFaker.Generate()
+            studios: AniListBrowseSeasonStudiosFaker.Generate()
         ));
 
     public static Faker<GetBrowseSection_TopLastSeason_Media_Media> AniListBrowseTopLastSeasonAnimeFaker => new Faker<GetBrowseSection_TopLastSeason_Media_Media>()
         .CustomInstantiator(f => new GetBrowseSection_TopLastSeason_Media_Media(
             id: f.Random.Number(1, 10000),
-            title: AniListTitleFaker.Generate(),
-            coverImage: AniListCoverImageFaker.Generate(),
+            title: AniListBrowseSeasonTitleFaker.Generate(),
+            coverImage: AniListBrowseSeasonCoverImageFaker.Generate(),
             startDate: null,
             endDate: null,
             bannerImage: f.Image.PlaceholderUrl(1900, 400),
@@ -118,14 +118,14 @@ public static class AniListProviderFakers
             isAdult: f.Random.Bool(),
             averageScore: f.Random.Number(0, 100),
             nextAiringEpisode: null,
-            studios: AniListStudiosFaker.Generate()
+            studios: AniListBrowseSeasonStudiosFaker.Generate()
         ));
 
     public static Faker<GetBrowseSection_Top_Media_Media> AniListBrowseTopAnimeFaker => new Faker<GetBrowseSection_Top_Media_Media>()
         .CustomInstantiator(f => new GetBrowseSection_Top_Media_Media(
             id: f.Random.Number(1, 10000),
-            title: AniListTitleFaker.Generate(),
-            coverImage: AniListCoverImageFaker.Generate(),
+            title: AniListBrowseSeasonTitleFaker.Generate(),
+            coverImage: AniListBrowseSeasonCoverImageFaker.Generate(),
             startDate: null,
             endDate: null,
             bannerImage: f.Image.PlaceholderUrl(1900, 400),
@@ -141,11 +141,18 @@ public static class AniListProviderFakers
             isAdult: f.Random.Bool(),
             averageScore: f.Random.Number(0, 100),
             nextAiringEpisode: null,
-            studios: AniListStudiosFaker.Generate()
+            studios: AniListBrowseSeasonStudiosFaker.Generate()
         ));
 
     public static Faker<GetAnimeById_Media_Title_MediaTitle> AniListTitleFaker => new Faker<GetAnimeById_Media_Title_MediaTitle>()
         .CustomInstantiator(f => new GetAnimeById_Media_Title_MediaTitle(
+            english: f.Lorem.Sentence(3, 3),
+            romaji: f.Lorem.Sentence(3, 3),
+            native: new Faker("ja").Lorem.Sentence(3, 3)
+        ));
+
+    public static Faker<GetSeasonalAnime_Page_Media_Title_MediaTitle> AniListSeasonalTitleFaker => new Faker<GetSeasonalAnime_Page_Media_Title_MediaTitle>()
+        .CustomInstantiator(f => new GetSeasonalAnime_Page_Media_Title_MediaTitle(
             english: f.Lorem.Sentence(3, 3),
             romaji: f.Lorem.Sentence(3, 3),
             native: new Faker("ja").Lorem.Sentence(3, 3)
@@ -167,5 +174,110 @@ public static class AniListProviderFakers
         .CustomInstantiator(f => new GetAnimeById_Media_Studios_Nodes_Studio(
             id: f.Random.Number(1, 10000),
             name: f.Company.CompanyName()
+        ));
+
+    // Seasonal anime fakers
+    public static Faker<GetSeasonalAnime_Page_Media_CoverImage_MediaCoverImage> AniListSeasonalCoverImageFaker => new Faker<GetSeasonalAnime_Page_Media_CoverImage_MediaCoverImage>()
+        .CustomInstantiator(f => new GetSeasonalAnime_Page_Media_CoverImage_MediaCoverImage(
+            extraLarge: f.Image.PlaceholderUrl(460, 650),
+            large: f.Image.PlaceholderUrl(230, 325),
+            color: f.Random.String2(7, "0123456789ABCDEF")
+        ));
+
+    public static Faker<GetSeasonalAnime_Page_Media_Studios_StudioConnection> AniListSeasonalStudiosFaker => new Faker<GetSeasonalAnime_Page_Media_Studios_StudioConnection>()
+        .CustomInstantiator(f => new GetSeasonalAnime_Page_Media_Studios_StudioConnection(
+            nodes: AniListSeasonalStudiosNodesFaker.GenerateBetween(1, 3).ToArray()
+        ));
+
+    public static Faker<GetSeasonalAnime_Page_Media_Studios_Nodes_Studio> AniListSeasonalStudiosNodesFaker => new Faker<GetSeasonalAnime_Page_Media_Studios_Nodes_Studio>()
+        .CustomInstantiator(f => new GetSeasonalAnime_Page_Media_Studios_Nodes_Studio(
+            id: f.Random.Number(1, 10000),
+            name: f.Company.CompanyName()
+        ));
+
+    // Browse section (Season) fakers
+    public static Faker<GetBrowseSection_Season_Media_Title_MediaTitle> AniListBrowseSeasonTitleFaker => new Faker<GetBrowseSection_Season_Media_Title_MediaTitle>()
+        .CustomInstantiator(f => new GetBrowseSection_Season_Media_Title_MediaTitle(
+            english: f.Lorem.Sentence(3, 3),
+            romaji: f.Lorem.Sentence(3, 3),
+            native: new Faker("ja").Lorem.Sentence(3, 3)
+        ));
+
+    public static Faker<GetBrowseSection_Season_Media_CoverImage_MediaCoverImage> AniListBrowseSeasonCoverImageFaker => new Faker<GetBrowseSection_Season_Media_CoverImage_MediaCoverImage>()
+        .CustomInstantiator(f => new GetBrowseSection_Season_Media_CoverImage_MediaCoverImage(
+            extraLarge: f.Image.PlaceholderUrl(460, 650),
+            large: f.Image.PlaceholderUrl(230, 325),
+            color: f.Random.String2(7, "0123456789ABCDEF")
+        ));
+
+    public static Faker<GetBrowseSection_Season_Media_Studios_StudioConnection> AniListBrowseSeasonStudiosFaker => new Faker<GetBrowseSection_Season_Media_Studios_StudioConnection>()
+        .CustomInstantiator(f => new GetBrowseSection_Season_Media_Studios_StudioConnection(
+            nodes: AniListBrowseSeasonStudiosNodesFaker.GenerateBetween(1, 3).ToArray()
+        ));
+
+    public static Faker<GetBrowseSection_Season_Media_Studios_Nodes_Studio> AniListBrowseSeasonStudiosNodesFaker => new Faker<GetBrowseSection_Season_Media_Studios_Nodes_Studio>()
+        .CustomInstantiator(f => new GetBrowseSection_Season_Media_Studios_Nodes_Studio(
+            id: f.Random.Number(1, 10000),
+            name: f.Company.CompanyName()
+        ));
+
+    // Browse section (NextSeason) fakers
+    public static Faker<GetBrowseSection_NextSeason_Media_Title_MediaTitle> AniListBrowseNextSeasonTitleFaker => new Faker<GetBrowseSection_NextSeason_Media_Title_MediaTitle>()
+        .CustomInstantiator(f => new GetBrowseSection_NextSeason_Media_Title_MediaTitle(
+            english: f.Lorem.Sentence(3, 3),
+            romaji: f.Lorem.Sentence(3, 3),
+            native: new Faker("ja").Lorem.Sentence(3, 3)
+        ));
+
+    public static Faker<GetBrowseSection_NextSeason_Media_CoverImage_MediaCoverImage> AniListBrowseNextSeasonCoverImageFaker => new Faker<GetBrowseSection_NextSeason_Media_CoverImage_MediaCoverImage>()
+        .CustomInstantiator(f => new GetBrowseSection_NextSeason_Media_CoverImage_MediaCoverImage(
+            extraLarge: f.Image.PlaceholderUrl(460, 650),
+            large: f.Image.PlaceholderUrl(230, 325),
+            color: f.Random.String2(7, "0123456789ABCDEF")
+        ));
+
+    public static Faker<GetBrowseSection_NextSeason_Media_Studios_StudioConnection> AniListBrowseNextSeasonStudiosFaker => new Faker<GetBrowseSection_NextSeason_Media_Studios_StudioConnection>()
+        .CustomInstantiator(f => new GetBrowseSection_NextSeason_Media_Studios_StudioConnection(
+            nodes: AniListBrowseSeasonStudiosNodesFaker.GenerateBetween(1, 3).ToArray()
+        ));
+
+    // Browse section (TopLastSeason) fakers
+    public static Faker<GetBrowseSection_TopLastSeason_Media_Title_MediaTitle> AniListBrowseTopLastSeasonTitleFaker => new Faker<GetBrowseSection_TopLastSeason_Media_Title_MediaTitle>()
+        .CustomInstantiator(f => new GetBrowseSection_TopLastSeason_Media_Title_MediaTitle(
+            english: f.Lorem.Sentence(3, 3),
+            romaji: f.Lorem.Sentence(3, 3),
+            native: new Faker("ja").Lorem.Sentence(3, 3)
+        ));
+
+    public static Faker<GetBrowseSection_TopLastSeason_Media_CoverImage_MediaCoverImage> AniListBrowseTopLastSeasonCoverImageFaker => new Faker<GetBrowseSection_TopLastSeason_Media_CoverImage_MediaCoverImage>()
+        .CustomInstantiator(f => new GetBrowseSection_TopLastSeason_Media_CoverImage_MediaCoverImage(
+            extraLarge: f.Image.PlaceholderUrl(460, 650),
+            large: f.Image.PlaceholderUrl(230, 325),
+            color: f.Random.String2(7, "0123456789ABCDEF")
+        ));
+
+    public static Faker<GetBrowseSection_TopLastSeason_Media_Studios_StudioConnection> AniListBrowseTopLastSeasonStudiosFaker => new Faker<GetBrowseSection_TopLastSeason_Media_Studios_StudioConnection>()
+        .CustomInstantiator(f => new GetBrowseSection_TopLastSeason_Media_Studios_StudioConnection(
+            nodes: AniListBrowseSeasonStudiosNodesFaker.GenerateBetween(1, 3).ToArray()
+        ));
+
+    // Browse section (Top) fakers
+    public static Faker<GetBrowseSection_Top_Media_Title_MediaTitle> AniListBrowseTopTitleFaker => new Faker<GetBrowseSection_Top_Media_Title_MediaTitle>()
+        .CustomInstantiator(f => new GetBrowseSection_Top_Media_Title_MediaTitle(
+            english: f.Lorem.Sentence(3, 3),
+            romaji: f.Lorem.Sentence(3, 3),
+            native: new Faker("ja").Lorem.Sentence(3, 3)
+        ));
+
+    public static Faker<GetBrowseSection_Top_Media_CoverImage_MediaCoverImage> AniListBrowseTopCoverImageFaker => new Faker<GetBrowseSection_Top_Media_CoverImage_MediaCoverImage>()
+        .CustomInstantiator(f => new GetBrowseSection_Top_Media_CoverImage_MediaCoverImage(
+            extraLarge: f.Image.PlaceholderUrl(460, 650),
+            large: f.Image.PlaceholderUrl(230, 325),
+            color: f.Random.String2(7, "0123456789ABCDEF")
+        ));
+
+    public static Faker<GetBrowseSection_Top_Media_Studios_StudioConnection> AniListBrowseTopStudiosFaker => new Faker<GetBrowseSection_Top_Media_Studios_StudioConnection>()
+        .CustomInstantiator(f => new GetBrowseSection_Top_Media_Studios_StudioConnection(
+            nodes: AniListBrowseSeasonStudiosNodesFaker.GenerateBetween(1, 3).ToArray()
         ));
 }
